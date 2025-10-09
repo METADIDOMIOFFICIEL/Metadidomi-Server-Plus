2aae52a95167b6b09ceaeae9df68641fef51dec0
METADIDOMIOFFICIEL <161984827+METADIDOMIOFFICIEL@users.noreply.github.com>
2025-10-09 11:27:29 +0100

    Delete Metadidomi-ServerPlus-Core/app/__pycache__/services directory

--- Metadidomi-ServerPlus-Core/app/__pycache__/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# Ce fichier rend le dossier 'services' importable comme un package Python.
-
-# Vous pouvez ajouter ici des initialisations de package ou des importations utiles.
-
-# Exemple :
-from .dynamic_routes import register_route, reload_routes
